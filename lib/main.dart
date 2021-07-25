214f06112277ce6d52c8b22307359fb732218f88
amanRecreates <amanofficial950@gmail.com>
2021-07-25 21:03:42 +0530

    custom font added

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,26 @@ import 'package:flutter/material.dart';
 void main() => runApp(MaterialApp(
   home: Scaffold(
     appBar: AppBar(
-      title: Text('AMAN APP'),
+      title: Text(
+        'AMAN APP',
+        style: TextStyle(
+          fontFamily: 'Poppins',
+          fontWeight: FontWeight.bold,
+        ),
+      ),
       centerTitle: true,
       backgroundColor: Colors.red[600],
     ),
     body: Center(
-      child: Text('Hey, This is my first app!'),
+      child: Text(
+        'Hey, This is my first app!',
+        style: TextStyle(
+          fontSize: 30.0,
+          fontWeight: FontWeight.bold,
+          color: Colors.grey[600],
+          fontFamily: 'Poppins',
+        ),
+      ),
     ),
     floatingActionButton: FloatingActionButton(
       onPressed: () {},
